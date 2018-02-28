a8d1ae52ea026fff4a4beba45618881f6abee1e3
midhatstam <midhatstam@gmail.com>
2018-02-28 14:17:40 +0300

    pycharm file commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import PostList, post_detail
 
 urlpatterns = [
     path(r'', PostList.as_view(template_name='blog/post_list.html'), name='blog_post_list'),
-    path(r'<year>/' r'^<month>/' r'^<slug>/', post_detail, {'parent_template': 'base.html'},
+    path(r'<year>/' r'<month>/' r'<slug>/', post_detail, {'parent_template': 'base.html'},
          name='blog_post_detail'),
 ]
