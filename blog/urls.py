4dfdb1df1ba8ba7e541a766323891e24868a33c9
github <midhatstam@gmail.com>
2018-03-19 14:09:17 +0300

    Fixing error

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PostList, post_detail, PostCreate, PostUpdate
+from .views import PostList, post_detail, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
     path(r'', PostList.as_view(template_name='blog/post_list.html'), name='blog_post_list'),
